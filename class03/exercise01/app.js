ff91ef1df213caf0c6e2762a2ad19428c9bd7cac
Patrick Seelinger <Patrick Seelinger>
2016-01-20 17:01:43 -0500

    exercise01 done

--- class03/exercise01/app.js
+++ class03/exercise01/app.js
@@ -2,10 +2,10 @@ function farToCel(tempFar){
     var tempCel = Math.floor((tempFar - 32) * (5/9));
     
     if (tempCel < 10) {
-        console.log("The current temperature is " + tempCel + " Celsius. It's cold outside.");
+        console.log("The current temperature is " + tempCel + " degrees Celsius. It's cold outside.");
     }else if (tempCel >= 10 && tempCel <= 25 ) {
-        console.log("The current temperature is " + tempCel + " Celsius. It's fair outside.");
+        console.log("The current temperature is " + tempCel + " degrees Celsius. It's fair outside.");
     }else{
-        console.log("The current temperature is " + tempCel + " Celsius. It's hot outside.");
+        console.log("The current temperature is " + tempCel + " degrees Celsius. It's hot outside.");
     }
 }
